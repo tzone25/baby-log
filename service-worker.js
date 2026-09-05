2681bef807cd0996ddf00066755461205cc7c26f
tzone25 <ironline.co.brand@gmail.com>
2026-09-06 08:13:36 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,57 @@
-const CACHE_NAME = "baby-log-v1";
+const CACHE_NAME = "baby-log-v4";
 
 const APP_FILES = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./service-worker.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
   );
-
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
       )
-    )
+      .then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
-      .then(cached => {
-        return cached || fetch(event.request);
+    fetch(event.request)
+      .then(response => {
+        if (response && response.status === 200) {
+          const responseClone = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+
+        return response;
       })
-      .catch(() => caches.match("./index.html"))
+      .catch(() =>
+        caches.match(event.request)
+          .then(cached => {
+            return cached || caches.match("./index.html");
+          })
+      )
   );
 });
